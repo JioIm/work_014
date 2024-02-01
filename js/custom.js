0a3da7a846cc215481990db0e8a965ff999f9a5a
Jio Im <148739315+JioIm@users.noreply.github.com>
2024-02-01 18:15:41 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,12 @@
 $(function () {
     $('.main_slide').slick({
         arrows: false,
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        // speed: 400,
+        fade: true,
+        cssEase: 'linear'
     })
 })
